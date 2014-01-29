2a99c99cef5d4cf5dcba314e905ff93f34f43486
GAFuller <gareth.a.f@gmail.com>
2014-01-29 00:37:13 +0000

    custom stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :set_meta_data
+  
+  def set_meta_data(title="App Title", keywords="app, key, words", description="App description.")
+    @meta_title, @meta_keywords, @meta_description = title, keywords, description
+  end
 end
